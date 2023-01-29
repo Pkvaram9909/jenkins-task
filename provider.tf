6a9188b45c2508be1edb74ef3f83625a877aa5ae
Pkvaram9909 <123714492+Pkvaram9909@users.noreply.github.com>
2023-01-29 17:33:59 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA4C2BV6GPVCLFUWM2"
+  secret_key = "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"
+}
